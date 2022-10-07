d97b4a1fdfd0ddc39900d0b9536a437f9befb540
Elina1105 <74726948+Elina1105@users.noreply.github.com>
2022-10-07 20:03:46 +0300

    Initial commit

---
+++ js/task-02.js
@@ -0,0 +1,34 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+
+const ingredientsList = document.querySelector("#ingredients");
+
+// console.log(ingredientsList);
+
+const itemList = ingredients.map(ingredient => {
+    const item = document.createElement("li");
+    item.classList.add("item");
+    item.textContent = `${ingredient}`;
+    return item;
+  })
+
+// console.log(itemList);
+
+ingredientsList.append(...itemList)
+console.log(ingredientsList);
+
+
+
+
+
+
+
+
+
---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const inputName = document.querySelector("#name-input");
+const spanName = document.querySelector("#name-output");
+
+inputName.addEventListener("input", (event) =>
+{
+    if (event.currentTarget.value === "") {
+        spanName.textContent = "Anomymus"
+    }
+    else { spanName.textContent = event.currentTarget.value }
+});
+
